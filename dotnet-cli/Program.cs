c249977ff3fef3b065beeebafa1111d5036a45fa
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-12-20 13:43:21 -0600

    quickstart for dotnet cli usage

---
+++ dotnet-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using StoryTeller;
+
+namespace dotnet_cli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            StorytellerAgent.Run(args);
+        }
+    }
+}
