3c235a9c2afdc5bdab41f75bf87f4b2e0238832f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-01-23 18:28:34 -0600

    csproj file quickstart is working

---
+++ classic-csproj/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace classic_csproj
+{
+    public class Calculator
+    {
+        public double Value { get; set; }
+
+        public void MultiplyBy(double value)
+        {
+            Value *= value;
+        }
+
+        public void DivideBy(double value)
+        {
+            Value /= value;
+        }
+
+        public void Add(double value)
+        {
+            Value += value;
+        }
+
+        public void Subtract(double value)
+        {
+            Value -= value;
+        }
+    }
+}
---
+++ classic-csproj/storyteller.cmd
@@ -0,0 +1,1 @@
+packages\StorytellerRunnerCsproj.4.0.0.463\tools\StorytellerRunner.exe %* --app-domain
